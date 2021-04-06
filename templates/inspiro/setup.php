83765e3360777c2235d1cb8774540ac5037c1218
vicolaspetru <vicolaspetru@gmail.com>
2021-04-06 17:51:13 +0300

    Added templates with setup file

---
+++ templates/inspiro/setup.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Theme template files setup
+ *
+ * @since 1.0.0
+ * @package WPZOOM_Beaver_Builder_Templates
+ */
+
+// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
+$template_files = array(
+	'default.dat',
+	'about.dat',
+	'services.dat',
+);
